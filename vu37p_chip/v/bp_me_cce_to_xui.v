97145931ead00c00aab232a31fbaece3feb09778
Paul Gao <gaozihou1@gmail.com>
2020-03-30 21:55:44 -0700

    vu37p_chip utilizing dual hbm channels lower clock frequency to speedup routing

--- vu37p_chip/v/bp_me_cce_to_xui.v
+++ vu37p_chip/v/bp_me_cce_to_xui.v
@@ -1,14 +1,12 @@
-/**
- * bp_me_cce_to_wormhole_link_master.v
- */
- 
-`include "bp_mem_wormhole.vh"
 
 module bp_me_cce_to_xui
+
  import bp_cce_pkg::*;
  import bp_common_pkg::*;
  import bp_common_aviary_pkg::*;
  import bp_me_pkg::*;
+ import bsg_cache_pkg::*;
+ 
  #(parameter bp_params_e bp_params_p = e_bp_inv_cfg
    `declare_bp_proc_params(bp_params_p)
    `declare_bp_me_if_widths(paddr_width_p, cce_block_width_p, lce_id_width_p, lce_assoc_p)
@@ -17,7 +15,12 @@ module bp_me_cce_to_xui
    , parameter cord_width_p = "inv"
    , parameter cid_width_p  = "inv"
    , parameter len_width_p  = "inv"
-
+   
+   , localparam cache_addr_width_p = 30 - `BSG_SAFE_CLOG2(1) // one cache_dma
+   , localparam axi_id_width_p     = 1
+   , localparam axi_addr_width_p   = 30
+   , localparam axi_data_width_p   = 256
+   , localparam axi_burst_len_p    = 2
    , localparam bsg_ready_and_link_sif_width_lp = `bsg_ready_and_link_sif_width(flit_width_p)
    )
   (input                                 clk_i
@@ -47,9 +50,230 @@ module bp_me_cce_to_xui
    , input                               app_rd_data_end_i
    );
    
-   assign mem_cmd_ready_o = 1'b1;
-   assign mem_resp_o = '0;
-   assign mem_resp_v_o = 1'b0;
+  // Do not process commands before apb_complete
+  wire apb_complete;
+  
+  wire [21:0]s_apb_paddr = '0;
+  wire s_apb_penable = 1'b0;
+  wire [31:0]s_apb_prdata;
+  wire s_apb_pready;
+  wire s_apb_psel = 1'b0;
+  wire s_apb_pslverr;
+  wire [31:0]s_apb_pwdata = '0;
+  wire s_apb_pwrite = 1'b0;
+
+  wire [29:0]s_axi_araddr;
+  wire [1:0]s_axi_arburst;
+  wire [3:0]s_axi_arcache;
+  wire [0:0]s_axi_arid;
+  wire [7:0]s_axi_arlen;
+  wire [0:0]s_axi_arlock;
+  wire [2:0]s_axi_arprot;
+  wire [3:0]s_axi_arqos;
+  wire s_axi_arready;
+  wire [3:0]s_axi_arregion;
+  wire [2:0]s_axi_arsize;
+  wire s_axi_arvalid;
+
+  wire [29:0]s_axi_awaddr;
+  wire [1:0]s_axi_awburst;
+  wire [3:0]s_axi_awcache;
+  wire [0:0]s_axi_awid;
+  wire [7:0]s_axi_awlen;
+  wire [0:0]s_axi_awlock;
+  wire [2:0]s_axi_awprot;
+  wire [3:0]s_axi_awqos;
+  wire s_axi_awready;
+  wire [3:0]s_axi_awregion;
+  wire [2:0]s_axi_awsize;
+  wire s_axi_awvalid;
+
+  wire [0:0]s_axi_bid;
+  wire s_axi_bready;
+  wire [1:0]s_axi_bresp;
+  wire s_axi_bvalid;
+
+  wire [255:0]s_axi_rdata;
+  wire [0:0]s_axi_rid;
+  wire s_axi_rlast;
+  wire s_axi_rready;
+  wire [1:0]s_axi_rresp;
+  wire s_axi_rvalid;
+
+  wire [255:0]s_axi_wdata;
+  wire s_axi_wlast;
+  wire s_axi_wready;
+  wire [31:0]s_axi_wstrb;
+  wire s_axi_wvalid;
+   
+  // CCE to cache dma
+  `declare_bsg_cache_dma_pkt_s(paddr_width_p);
+  
+  bsg_cache_dma_pkt_s dma_pkt_lo;
+  logic dma_pkt_v_lo, dma_pkt_yumi_li;
+  
+  logic [dword_width_p-1:0] dma_data_li;
+  logic dma_data_v_li, dma_data_ready_lo;
+  
+  logic [dword_width_p-1:0] dma_data_lo;
+  logic dma_data_v_lo, dma_data_yumi_li;
+  
+  logic [cache_addr_width_p+1-1:0] cache_dma_pkt_lo;
+    assign cache_dma_pkt_lo = {dma_pkt_lo.write_not_read, dma_pkt_lo[cache_addr_width_p-1:0]};
+
+  bp_me_cce_to_cache_dma
+ #(.bp_params_p(bp_params_p)
+  ) mem_to_dma
+  (.clk_i           (clk_i)
+  ,.reset_i         (reset_i)
+                    
+  ,.dma_pkt_o       (dma_pkt_lo)
+  ,.dma_pkt_v_o     (dma_pkt_v_lo)
+  ,.dma_pkt_yumi_i  (dma_pkt_yumi_li)
+
+  ,.dma_data_i      (dma_data_li)
+  ,.dma_data_v_i    (dma_data_v_li)
+  ,.dma_data_ready_o(dma_data_ready_lo)
+
+  ,.dma_data_o      (dma_data_lo)
+  ,.dma_data_v_o    (dma_data_v_lo)
+  ,.dma_data_yumi_i (dma_data_yumi_li)
+
+  ,.mem_cmd_i       (mem_cmd_i)
+  ,.mem_cmd_v_i     (mem_cmd_v_i & apb_complete)
+  ,.mem_cmd_yumi_o  (mem_cmd_ready_o)
+
+  ,.mem_resp_o      (mem_resp_o)
+  ,.mem_resp_v_o    (mem_resp_v_o)
+  ,.mem_resp_ready_i(mem_resp_yumi_i)
+  );
+
+  // s_axi port
+  // not supported
+  assign s_axi_arqos    = '0;
+  assign s_axi_arregion = '0;
+  assign s_axi_awqos    = '0;
+  assign s_axi_awregion = '0;
+
+  bsg_cache_to_axi 
+ #(.addr_width_p         (cache_addr_width_p)
+  ,.block_size_in_words_p(cce_block_width_p/dword_width_p)
+  ,.data_width_p         (dword_width_p)
+  ,.num_cache_p          (1)
+  ,.tag_fifo_els_p       (num_core_p)
+
+  ,.axi_id_width_p       (axi_id_width_p)
+  ,.axi_addr_width_p     (axi_addr_width_p)
+  ,.axi_data_width_p     (axi_data_width_p)
+  ,.axi_burst_len_p      (axi_burst_len_p)
+  ) cache_to_axi 
+  (.clk_i  (clk_i)
+  ,.reset_i(reset_i)
+  
+  ,.dma_pkt_i       (cache_dma_pkt_lo)
+  ,.dma_pkt_v_i     (dma_pkt_v_lo)
+  ,.dma_pkt_yumi_o  (dma_pkt_yumi_li)
+  
+  ,.dma_data_o      (dma_data_li)
+  ,.dma_data_v_o    (dma_data_v_li)
+  ,.dma_data_ready_i(dma_data_ready_lo)
+  
+  ,.dma_data_i      (dma_data_lo)
+  ,.dma_data_v_i    (dma_data_v_lo)
+  ,.dma_data_yumi_o (dma_data_yumi_li)
+
+  ,.axi_awid_o      (s_axi_awid)
+  ,.axi_awaddr_o    (s_axi_awaddr)
+  ,.axi_awlen_o     (s_axi_awlen)
+  ,.axi_awsize_o    (s_axi_awsize)
+  ,.axi_awburst_o   (s_axi_awburst)
+  ,.axi_awcache_o   (s_axi_awcache)
+  ,.axi_awprot_o    (s_axi_awprot)
+  ,.axi_awlock_o    (s_axi_awlock)
+  ,.axi_awvalid_o   (s_axi_awvalid)
+  ,.axi_awready_i   (s_axi_awready)
+                    
+  ,.axi_wdata_o     (s_axi_wdata)
+  ,.axi_wstrb_o     (s_axi_wstrb)
+  ,.axi_wlast_o     (s_axi_wlast)
+  ,.axi_wvalid_o    (s_axi_wvalid)
+  ,.axi_wready_i    (s_axi_wready)
+                    
+  ,.axi_bid_i       (s_axi_bid)
+  ,.axi_bresp_i     (s_axi_bresp)
+  ,.axi_bvalid_i    (s_axi_bvalid)
+  ,.axi_bready_o    (s_axi_bready)
+                    
+  ,.axi_arid_o      (s_axi_arid)
+  ,.axi_araddr_o    (s_axi_araddr)
+  ,.axi_arlen_o     (s_axi_arlen)
+  ,.axi_arsize_o    (s_axi_arsize)
+  ,.axi_arburst_o   (s_axi_arburst)
+  ,.axi_arcache_o   (s_axi_arcache)
+  ,.axi_arprot_o    (s_axi_arprot)
+  ,.axi_arlock_o    (s_axi_arlock)
+  ,.axi_arvalid_o   (s_axi_arvalid)
+  ,.axi_arready_i   (s_axi_arready)
+                    
+  ,.axi_rid_i       (s_axi_rid)
+  ,.axi_rdata_i     (s_axi_rdata)
+  ,.axi_rresp_i     (s_axi_rresp)
+  ,.axi_rlast_i     (s_axi_rlast)
+  ,.axi_rvalid_i    (s_axi_rvalid)
+  ,.axi_rready_o    (s_axi_rready)
+  );
+
+  design_2 design_2_i
+       (.apb_complete(apb_complete),
+        .clk(clk_i),
+        .reset(reset_i),
+        .s_apb_paddr(s_apb_paddr),
+        .s_apb_penable(s_apb_penable),
+        .s_apb_prdata(s_apb_prdata),
+        .s_apb_pready(s_apb_pready),
+        .s_apb_psel(s_apb_psel),
+        .s_apb_pslverr(s_apb_pslverr),
+        .s_apb_pwdata(s_apb_pwdata),
+        .s_apb_pwrite(s_apb_pwrite),
+        .s_axi_araddr(s_axi_araddr),
+        .s_axi_arburst(s_axi_arburst),
+        .s_axi_arcache(s_axi_arcache),
+        .s_axi_arid(s_axi_arid),
+        .s_axi_arlen(s_axi_arlen),
+        .s_axi_arlock(s_axi_arlock),
+        .s_axi_arprot(s_axi_arprot),
+        .s_axi_arqos(s_axi_arqos),
+        .s_axi_arready(s_axi_arready),
+        .s_axi_arregion(s_axi_arregion),
+        .s_axi_arsize(s_axi_arsize),
+        .s_axi_arvalid(s_axi_arvalid),
+        .s_axi_awaddr(s_axi_awaddr),
+        .s_axi_awburst(s_axi_awburst),
+        .s_axi_awcache(s_axi_awcache),
+        .s_axi_awid(s_axi_awid),
+        .s_axi_awlen(s_axi_awlen),
+        .s_axi_awlock(s_axi_awlock),
+        .s_axi_awprot(s_axi_awprot),
+        .s_axi_awqos(s_axi_awqos),
+        .s_axi_awready(s_axi_awready),
+        .s_axi_awregion(s_axi_awregion),
+        .s_axi_awsize(s_axi_awsize),
+        .s_axi_awvalid(s_axi_awvalid),
+        .s_axi_bid(s_axi_bid),
+        .s_axi_bready(s_axi_bready),
+        .s_axi_bresp(s_axi_bresp),
+        .s_axi_bvalid(s_axi_bvalid),
+        .s_axi_rdata(s_axi_rdata),
+        .s_axi_rid(s_axi_rid),
+        .s_axi_rlast(s_axi_rlast),
+        .s_axi_rready(s_axi_rready),
+        .s_axi_rresp(s_axi_rresp),
+        .s_axi_rvalid(s_axi_rvalid),
+        .s_axi_wdata(s_axi_wdata),
+        .s_axi_wlast(s_axi_wlast),
+        .s_axi_wready(s_axi_wready),
+        .s_axi_wstrb(s_axi_wstrb),
+        .s_axi_wvalid(s_axi_wvalid));
    
    assign app_addr_o = '0;
    assign app_cmd_o = '0;
