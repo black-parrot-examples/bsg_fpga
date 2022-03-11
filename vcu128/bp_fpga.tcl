e31fd8fc1d3137477296fd61ea24c7b1f0b9e2ad
Mark Wyse <muwyse@gmail.com>
2022-03-11 10:25:45 -0800

    remove module from extra files, in flist

--- vcu128/bp_fpga.tcl
+++ vcu128/bp_fpga.tcl
@@ -210,7 +210,6 @@ set files [list \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi_rx.v"] \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi_tx.v"] \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi.v"] \
- [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_misc/bsg_counter_clock_downsample.v"] \
  [file normalize "${origin_dir}/v/design_1_wrapper.v"] \
  [file normalize "${origin_dir}/sdk/install/ucode/moesif.mem"] \
 ]
@@ -247,11 +246,6 @@ set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
 set file "$blackparrot_dir/external/basejump_stl/bsg_cache/bsg_cache_to_axi.v"
-set file [file normalize $file]
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
-set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
-
-set file "$blackparrot_dir/external/basejump_stl/bsg_misc/bsg_counter_clock_downsample.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
