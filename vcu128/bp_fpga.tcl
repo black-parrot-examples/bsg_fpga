e1ef7c57f118464cf0a450800615221ed017f26e
Mark Wyse <muwyse@gmail.com>
2022-04-07 11:01:40 -0700

    design_1_wrapper.v copy into project dir to allow concurrent builds on unique configs

--- vcu128/bp_fpga.tcl
+++ vcu128/bp_fpga.tcl
@@ -195,7 +195,7 @@ set files [list \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi_rx.v"] \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi_tx.v"] \
  [file normalize "${blackparrot_dir}/external/basejump_stl/bsg_cache/bsg_cache_to_axi.v"] \
- [file normalize "${origin_dir}/v/design_1_wrapper.v"] \
+ [file normalize "${proj_dir}/design_1_wrapper.v"] \
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -234,7 +234,7 @@ set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
 
-set file "$origin_dir/v/design_1_wrapper.v"
+set file "$proj_dir/design_1_wrapper.v"
 set file [file normalize $file]
 set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file"]]
 set_property -name "file_type" -value "SystemVerilog" -objects $file_obj
